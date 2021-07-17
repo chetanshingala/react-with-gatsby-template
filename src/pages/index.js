bd60adb6e16aad24a267059e4b20427be065b3ab
chetanshingala <chetanshingala99@gmail.com>
2021-07-17 08:40:00 +0530

    created gatsby template

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code react app</h1>
+        <p>Complete course about the tools and design system Prototype and build apps with react</p>
+        <Link to="/page-2/">What the video</Link> 
+      </div>
+    </div>
+
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
